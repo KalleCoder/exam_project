ccc635e5e7252aabfb8d9a331036ecc0602ed4b6
KalleCoder <kallehandin@gmail.com>
2024-12-17 10:22:04 +0100

    resturctured and fixed the ui

---
+++ client/lib/communication/communication.py
@@ -0,0 +1,8 @@
+# press the button in main --> pack it into buffer in security --> send request
+# recieve buffer --> unpack it in the security --> show the data
+
+# communication class only gets the data
+
+# security unpacks or packs
+
+# main just shows the gui
